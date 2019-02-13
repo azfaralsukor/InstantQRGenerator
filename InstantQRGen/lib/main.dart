b1a3b9c4ac88afceed1a735955f7ce69cbebf0f0
Satoshi Nakamoto <azfaralsukor@gmail.com>
2019-02-13 21:13:39 +0800

    Second Version

--- InstantQRGen/lib/main.dart
+++ InstantQRGen/lib/main.dart
@@ -5,7 +5,6 @@ import 'screens/main.screen.dart';
 void main() => runApp(App());
 
 class App extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- InstantQRGen/lib/screens/main.screen.dart
+++ InstantQRGen/lib/screens/main.screen.dart
@@ -6,137 +6,18 @@ class MainScreen extends StatefulWidget {
   _MainScreenState createState() => _MainScreenState();
 }
 
+String _dataString = 'Hello World',
+    _active = 'Email',
+    _inputEmailText,
+    _inputErrorText;
+final TextEditingController _emailController = TextEditingController(),
+    _phoneController = TextEditingController(),
+    _wifiController = TextEditingController();
+bool _obscureTextLogin = true;
+
 class _MainScreenState extends State<MainScreen> {
   static const double _topSectionTopPadding = 50.0;
   static const double _topSectionBottomPadding = 20.0;
-
-  String _dataString = 'Hello World', _active = 'Email', _inputErrorText;
-  final TextEditingController _emailController = TextEditingController(),
-      _phoneController = TextEditingController(),
-      _wifiController = TextEditingController();
-
-  bool _obscureTextLogin = true;
-
-  void _toggleLogin() {
-    setState(() {
-      _obscureTextLogin = !_obscureTextLogin;
-    });
-    print('_obsureTextLogin: '+_obscureTextLogin.toString());
-  }
-
-  void _showSettingsDialog() {
-    showDialog<void>(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Icon(Icons.settings),
-          content: Container(
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                Row(
-                  mainAxisSize: MainAxisSize.max,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Expanded(
-                      child: TextField(
-                        controller: _emailController,
-                        decoration: InputDecoration(
-                          icon: const Icon(Icons.email),
-                          errorText: _inputErrorText,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                Row(
-                  mainAxisSize: MainAxisSize.max,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Expanded(
-                      child: TextField(
-                        keyboardType: const TextInputType.numberWithOptions(
-                          decimal: false,
-                          signed: false,
-                        ),
-                        controller: _phoneController,
-                        decoration: InputDecoration(
-                          icon: const Icon(Icons.phone),
-                          errorText: _inputErrorText,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                Row(
-                  mainAxisSize: MainAxisSize.max,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Expanded(
-                      child: TextField(
-                        controller: _wifiController,
-                        obscureText: _obscureTextLogin,
-                        decoration: InputDecoration(
-                          icon: const Icon(Icons.wifi),
-                          errorText: _inputErrorText,
-                          suffixIcon: GestureDetector(
-                            onTap: _toggleLogin,
-                            child: const Icon(
-                              Icons.remove_red_eye,
-                              size: 15.0,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ])),
-          actions: <Widget>[
-            FlatButton(
-              child: const Icon(Icons.clear),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-            FlatButton(
-              child: const Icon(Icons.check),
-              onPressed: () {
-                switch (_active) {
-                  case 'Email':
-                    {
-                      setState(() {
-                        _dataString = _emailController.text;
-                        _inputErrorText = null;
-                      });
-                    }
-                    break;
-                  case 'Phone':
-                    {
-                      setState(() {
-                        _dataString = _phoneController.text;
-                        _inputErrorText = null;
-                      });
-                    }
-                    break;
-                  default:
-                    {
-                      setState(() {
-                        _dataString = _wifiController.text;
-                        _inputErrorText = null;
-                      });
-                    }
-                    break;
-                }
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -165,7 +46,7 @@ class _MainScreenState extends State<MainScreen> {
             color: Theme.of(context).primaryColor,
           ),
         ),
-        body: TabBarView(children: [_qrWidget(), _cameraWidget()]),
+        body: TabBarView(children: <Widget>[_qrWidget(), _cameraWidget()]),
       ),
     );
   }
@@ -190,7 +71,7 @@ class _MainScreenState extends State<MainScreen> {
             ),
           ),
           Text(
-            _dataString,
+            _active == 'Wifi' ? '' : _dataString,
             style: TextStyle(
                 fontSize: 20,
                 fontFamily: 'Arial',
@@ -227,7 +108,7 @@ class _MainScreenState extends State<MainScreen> {
                 onPressed: () {
                   setState(() {
                     _dataString = _emailController.text;
-                    _inputErrorText = null;
+                    _inputEmailText = null;
                     _active = 'Email';
                   });
                 },
@@ -291,12 +172,130 @@ class _MainScreenState extends State<MainScreen> {
     );
   }
 
+  void _showSettingsDialog() {
+    showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return _Settings();
+      },
+    );
+  }
+
   Widget _cameraWidget() {
     return Container(
       color: Colors.black,
       child: Column(
         children: <Widget>[],
       ),
+    );
+  }
+}
+
+class _Settings extends StatefulWidget {
+  @override
+  State<_Settings> createState() {
+    return _SettingsState();
+  }
+}
+
+class _SettingsState extends State<_Settings> {
+  void _toggleLogin() {
+    setState(() {
+      _obscureTextLogin = !_obscureTextLogin;
+    });
+  }
+
+  void _emailVal(String x) {
+    final RegExp exp =
+        RegExp(r'(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)');
+    final Iterable<Match> matches = exp.allMatches(x);
+    setState(() {
+      if (matches.isEmpty) {
+        _inputEmailText = '';
+      } else {
+        _inputEmailText = null;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Icon(Icons.settings),
+      content: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            TextField(
+              controller: _emailController,
+              onChanged: _emailVal,
+              decoration: InputDecoration(
+                icon: const Icon(Icons.email),
+                errorText: _inputEmailText,
+                errorStyle: const TextStyle(fontSize: 0)
+              ),
+            ),
+            TextField(
+              keyboardType: TextInputType.phone,
+              controller: _phoneController,
+              decoration: InputDecoration(
+                icon: const Icon(Icons.phone),
+                errorText: _inputErrorText,
+              ),
+            ),
+            TextField(
+              controller: _wifiController,
+              obscureText: _obscureTextLogin,
+              decoration: InputDecoration(
+                icon: const Icon(Icons.wifi),
+                errorText: _inputErrorText,
+                suffixIcon: GestureDetector(
+                  onTap: _toggleLogin,
+                  child: const Icon(
+                    Icons.remove_red_eye,
+                    size: 15.0,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      actions: <Widget>[
+        FlatButton(
+          child: const Icon(Icons.clear),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        FlatButton(
+          child: const Icon(Icons.check),
+          onPressed: () {
+            switch (_active) {
+              case 'Email':
+                {
+                  _dataString = _emailController.text;
+                  _inputErrorText = null;
+                }
+                break;
+              case 'Phone':
+                {
+                  _dataString = _phoneController.text;
+                  _inputErrorText = null;
+                }
+                break;
+              default:
+                {
+                  _dataString = _wifiController.text;
+                  _inputErrorText = null;
+                }
+                break;
+            }
+            Navigator.of(context).pop();
+          },
+        ),
+      ],
     );
   }
 }
